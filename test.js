4328d698f36144d922a2e1933d287e774dbc5561
apatel922 <59633697+apatel922@users.noreply.github.com>
2020-02-09 02:53:30 -0500

    All base reqs solved
    Also solved for password history

--- test.js
+++ test.js
@@ -1,46 +1,59 @@
 
 var passList = document.querySelector("#list");
+var passString = document.querySelector("#keyGen");
 
 
-// generate random pass
-function create () {
+//window.onload = function () {
+    // generate random pass
+    function create () {
 
-    // randomize password length (8-16 char)
-    var passLength = Math.floor((Math.random() * 9) + 8);
-    //console.log(passLength);
+        // randomize password length (8-16 char)
+        var passLength = Math.floor((Math.random() * 9) + 8);
+        console.log(passLength);
 
-    // all possible characters as a string
-    var charLibrary = "abcdefghijklmnopqrstuvwxyz!@#$%^&*?()<>[]{}ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        // all possible characters as a string
+        var charLibrary = "abcdefghijklmnopqrstuvwxyz!@#$%^&*?()<>[]{}ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    // blank string for generated password & blank array
-    var password = "";
-    var history = [];
+        // blank string for generated password & blank array
+        var password = "";
+        // var history = [];
 
-    // for loop to create password
-    for (var i = 0; i < passLength; i++) {
-        password = password + charLibrary.charAt(Math.floor(Math.random() * Math.floor(charLibrary.length - 1)));
+        // for loop to create password
+        for (var i = 0; i < passLength; i++) {
+            password = password + charLibrary.charAt(Math.floor(Math.random() * Math.floor(charLibrary.length - 1)));
+        }
+
+        // add password to text field
+        document.getElementById("keyGen").value = password;
+
+        // create list item in modal & populate with latest password
+        var li = document.createElement("li");
+        var text = document.createTextNode(password);
+        console.log(text);
+        li.append(text);
+        document.getElementById("list").appendChild(li);
+
     }
+//};
 
-    // add password to text field
-    document.getElementById("keyGen").value = password;
 
-    // create list item in modal & populate with latest password
-    history.push(password);
-    console.log(history);
-
+// copy to clipboard
+function copy () {
+//  var copyValue = document.passString;
+    var copyValue = document.getElementById("keyGen");
+    console.log(copyValue);
+    copyValue.select();
+    document.execCommand("copy");
 }
-
-
-
 
 
 
 // Generate a random password
-function genPassword(event) {
+// function genPassword(event) {
 
-    for (var i = 0, n = charLibrary.length; i < passLength; i++) {
-        returnValue = returnValue + charLibrary.charAt(Math.floor(Math.random() * n));
-    }
-    return returnValue;
-
-}
+//     for (var i = 0, n = charLibrary.length; i < passLength; i++) {
+//         returnValue = returnValue + charLibrary.charAt(Math.floor(Math.random() * n));
+//     }
+//     console.log(returnValue);
+//     return returnValue;
+// }
